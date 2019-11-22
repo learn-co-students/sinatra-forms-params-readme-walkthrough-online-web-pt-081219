8f5f7c68264af85c84dfea4165132b4378821354
JASPMobus <mobusj@live.com>
2019-11-21 17:05:39 -0800

    Done.

--- app.rb
+++ app.rb
@@ -1,11 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  configure do
+    set :views, "views"
+  end
 
   get '/food_form' do
     erb :food_form
   end
 
   # Add your post route and action below
-
+  post "/food" do
+    @name = params[:name]
+    @favorite_food = params[:favorite_food]
+    
+    erb :food 
+  end
 end
