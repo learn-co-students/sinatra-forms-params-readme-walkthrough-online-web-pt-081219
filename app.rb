5842fcf642ad05bf08a9d4649cf55be11ba43d6f
cep104 <candicelizabethpeters@gmail.com>
2020-11-13 23:40:41 -0600

    added notes

--- app.rb
+++ app.rb
@@ -1,13 +1,17 @@
 require_relative 'config/environment'
-
+require 'pry'
 class App < Sinatra::Base
 
   get '/food_form' do
     erb :food_form
+    
+    
   end
 
   # Add your post route and action below
   post '/food' do
+    # binding.pry put binding.pry and look at what params are 
+    #params[:name] and params[:favorite_food]
     "My name is #{params[:name]}, and I love #{params[:favorite_food]}"
   end
 end
